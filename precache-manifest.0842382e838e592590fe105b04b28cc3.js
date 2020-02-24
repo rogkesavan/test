ef38a5d291a1be1b2c9f45cb3d4299f34b07d75c
rogkesavan <skesavane@gmail.com>
2020-02-24 17:41:41 +0530

    Updates

---
+++ precache-manifest.0842382e838e592590fe105b04b28cc3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e81c303aae8375218eaaa91efd4c0b9",
+    "url": "/test/index.html"
+  },
+  {
+    "revision": "eff9c43ae8e69e85b97d",
+    "url": "/test/static/css/main.07039201.chunk.css"
+  },
+  {
+    "revision": "de82a97555c7567cfbcb",
+    "url": "/test/static/js/2.8900bb6a.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/test/static/js/2.8900bb6a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eff9c43ae8e69e85b97d",
+    "url": "/test/static/js/main.aeaf8c0d.chunk.js"
+  },
+  {
+    "revision": "76157e87049cf6449a3d",
+    "url": "/test/static/js/runtime-main.6b649e9c.js"
+  },
+  {
+    "revision": "2cc2525d3edd235681727333db49d5ee",
+    "url": "/test/static/media/login-indesign.2cc2525d.png"
+  },
+  {
+    "revision": "c55819c4dba728837f134d044e01c6ea",
+    "url": "/test/static/media/profile.c55819c4.png"
+  }
+]);
